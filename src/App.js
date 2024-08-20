08a607365f155b604eb664b52bb2ccc2ccb19b75
Saurabh Shukla <saurabhshukla398@gmail.com>
2024-08-20 10:53:36 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 /* eslint-disable no-unused-vars */
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import Layout from './Components/Layout';
-import Home from './Components/Home';
-import ToDoList from './Components/ToDoList';
-import SendPasswordResetEmail from './Components/SendPasswordResetEmail';
-import ResetPassword from './Components/ResetPassword';
-import Contests from './Components/Contests';
-
+import Home from './Pages/Home';
+import ToDoList from './Pages/ToDoList';
+import Contests from './Pages/Contests';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <>
       <Routes>
-      <Route path='/' element={<Layout/>} >
-        <Route index element={<Home/>}/>
+      <Route path='/' element={<Home/>} >
+      <Route index element={<Home/>}/>
         <Route path='todolist' element={<ToDoList/>}/>
         <Route path='contests' element={<Contests/>}/>
       </Route>
-      <Route path='/sendpasswordresetemail' element={<SendPasswordResetEmail/>}/>
-      <Route path='/resetpassword' element={<ResetPassword/>}/>
     </Routes>
     </>
     
