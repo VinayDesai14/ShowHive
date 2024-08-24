b910a8dc7531bf95fe2aba84046b7161579f49c2
Saurabh Shukla <saurabhshukla398@gmail.com>
2024-08-24 10:42:09 +0530

    deleted server folder

--- Client/src/Components/Navbar.js
+++ Client/src/Components/Navbar.js
@@ -1,17 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
 import './Navbar.css';
 import Signup from '../Pages/Signup';
-const  NavbarComp = () => {
-  const [modal,setModalShow]=useState(false);
+
+const NavbarComp = () => {
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className="bg-body-tertiary navbar">
       <Container>
-        <Navbar.Brand href="#home">BookMyShow</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="#home" className="d-flex align-items-center">BookMyShow</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="navbar-toggler" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#movies">Movies</Nav.Link>
@@ -19,12 +18,13 @@ const  NavbarComp = () => {
             <Nav.Link href="#plays">Plays</Nav.Link>
             <Nav.Link href="#sports">Sports</Nav.Link>
           </Nav>
-          <Signup/>
-          {/* <Button variant='light' onClick={() => setModalShow(true)} >SignIn</Button> */}
+          <div className="ms-auto">
+            <Signup className='signup-btn' />
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default NavbarComp
+export default NavbarComp;
